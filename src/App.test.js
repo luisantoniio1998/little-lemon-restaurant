7e01c8d7e9d42b6168b80a8e8119f1e1ffddb41a
zemmsoares <zemmsoares@gmail.com>
2023-02-04 18:46:02 +0000

    unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,9 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import BookingForm from "./components/BookingForm";
 import { initializeTimes, updateTimes, state } from "./components/Reservation";
+import { fetchAPI } from "./API";
+
+const mockedUsedNavigate = jest.fn();
 
 test("Renders the BookingForm heading", () => {
   render(<BookingForm />);
@@ -10,13 +13,27 @@ test("Renders the BookingForm heading", () => {
 });
 
 test("initializeTimes validate that it returns expected value", () => {
-  const expectedResult = { availableTimes: ["17:00", "18:00", "19:00"] };
+  const expectedResult = { availableTimes: fetchAPI(new Date()) };
   const result = initializeTimes();
   expect(result).toEqual(expectedResult);
 });
 
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => jest.fn(),
+}));
+
 test("updateTimes", () => {
-  const state = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
-  const result = updateTimes(state, { type: "update_time", payload: state });
-  expect(result).toEqual(state);
+  const selectedDate = new Date("2022-06-01");
+  const availableTimes = updateTimes(selectedDate);
+
+  expect(Array.isArray(availableTimes)).toBe(true);
+  expect(availableTimes).toEqual([
+    "17:00",
+    "17:30",
+    "18:00",
+    "20:00",
+    "21:00",
+    "23:30",
+  ]);
 });
